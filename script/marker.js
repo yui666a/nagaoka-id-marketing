595ccdf1e9d65ac15ca9879d08f6e8d6ad9548b4
yui666a <haiso666@gmail.com>
2022-12-06 13:52:51 +0900

    pattern ファイルのパスを変更

--- script/marker.js
+++ script/marker.js
@@ -171,7 +171,7 @@ const AFrameScene = () => {
     //   <Camera />
     // </a-scene>
     <a-scene embedded arjs="debugUIEnabled:true;">
-    <a-marker type='pattern' url='./assets/marker/pattern-tradewaltz.patt'>
+    <a-marker type='pattern' url='/pattern-tradewaltz.patt'>
       <a-box position="0 0.5 0" wireframe="true"></a-box>
     </a-marker>
     <a-entity camera></a-entity>
