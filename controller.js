779d1b1248bac8474869bd0f36ba905b86112248
yui666a <haiso666@gmail.com>
2022-12-07 01:36:16 +0900

    a-sceneタグに複数のpropsを入力してみる

--- controller.js
+++ controller.js
@@ -1,23 +1,22 @@
 ////////////////////////////////////
 // 焦点を合わせるとSNSへ移動
 ////////////////////////////////////
-const layout = document.querySelector('#layout');
-layout.addEventListener('click', function () {
+const layout = document.querySelector("#layout");
+layout.addEventListener("click", function () {
   // location.href = "https://www.tradewaltz.com/";
-  console.log("visit \"https://www.tradewaltz.com/\"");
+  console.log("visit 'https://www.tradewaltz.com/'");
 });
-
 
 ////////////////////////////////////
 // 焦点を合わせると色変化
 ////////////////////////////////////
-var targetEl = document.querySelector("#target");
-targetEl.addEventListener("click", function () {
-    targetEl.setAttribute("material", {
-        color: "red"
-    });
-    console.log("clicked");
-});
+// var targetEl = document.querySelector("#target");
+// targetEl.addEventListener("click", function () {
+//     targetEl.setAttribute("material", {
+//         color: "red"
+//     });
+//     console.log("clicked");
+// });
 
 ////////////////////////////////////
 // タッチ機能の検証
@@ -41,20 +40,20 @@ targetEl.addEventListener("click", function () {
 //加速度センサの情報取得の許可
 ////////////////////////////////////
 document
-    .getElementById("request_permission")
-    .addEventListener("click", function () {
-        if (
-            DeviceMotionEvent &&
-            DeviceMotionEvent.requestPermission &&
-            typeof DeviceMotionEvent.requestPermission === "function"
-        ) {
-            DeviceMotionEvent.requestPermission();
-        }
-        if (
-            DeviceOrientationEvent &&
-            DeviceOrientationEvent.requestPermission &&
-            typeof DeviceOrientationEvent.requestPermission === "function"
-        ) {
-            DeviceOrientationEvent.requestPermission();
-        }
-    });
+  .getElementById("request_permission")
+  .addEventListener("click", function () {
+    if (
+      DeviceMotionEvent &&
+      DeviceMotionEvent.requestPermission &&
+      typeof DeviceMotionEvent.requestPermission === "function"
+    ) {
+      DeviceMotionEvent.requestPermission();
+    }
+    if (
+      DeviceOrientationEvent &&
+      DeviceOrientationEvent.requestPermission &&
+      typeof DeviceOrientationEvent.requestPermission === "function"
+    ) {
+      DeviceOrientationEvent.requestPermission();
+    }
+  });
